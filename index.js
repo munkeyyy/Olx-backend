854a4cd5cf0f2c1909cd4af7cf2fc6b7293fc629
munkeyyy <r2256308@gmail.com>
2024-05-01 12:54:14 +0530

    add async await in DB connection

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors"
+import cors from "cors";
 import userRouter from "./routers/user.router";
-import categoryRouter from"./routers/category.router";
-import subCategoryRouter from "./routers/subcategory.router"
-import brandRouter from "./routers/brand.router"
-import productRouter from './routers/product.router'
-import favRouter from "./routers/favourite.router"
+import categoryRouter from "./routers/category.router";
+import subCategoryRouter from "./routers/subcategory.router";
+import brandRouter from "./routers/brand.router";
+import productRouter from "./routers/product.router";
+import favRouter from "./routers/favourite.router";
 const app = express();
-app.use(express.json()) //body-parser (to read request body data)
+app.use(express.json()); //body-parser (to read request body data)
 
-app.use('/uploads', express.static('uploads'))
-app.use(cors())
+app.use("/uploads", express.static("uploads"));
+app.use(cors());
 
 const port = 8001;
 
-mongoose
-  .connect("mongodb://127.0.0.1/OLX")
-  .then(() => console.log("DB Connected!"))
-  .catch((err) => console.log(err));
+async function connectToDatabase() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1/OLX");
+    console.log("DB Connected!");
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
+}
 
+// Call the async function to connect to the database
+connectToDatabase();
 app.listen(port, () => {
   console.log("Server is running on port " + port);
 });
 
-app.use(process.env.URL_PREFIX+"users", userRouter);
-app.use(process.env.URL_PREFIX+"categories",categoryRouter);
-app.use(process.env.URL_PREFIX+"sub-categories",subCategoryRouter);
-app.use(process.env.URL_PREFIX+"brands",brandRouter);
-app.use(process.env.URL_PREFIX+"products",productRouter)
-app.use(process.env.URL_PREFIX+"favourites",favRouter)
+app.use(process.env.URL_PREFIX + "users", userRouter);
+app.use(process.env.URL_PREFIX + "categories", categoryRouter);
+app.use(process.env.URL_PREFIX + "sub-categories", subCategoryRouter);
+app.use(process.env.URL_PREFIX + "brands", brandRouter);
+app.use(process.env.URL_PREFIX + "products", productRouter);
+app.use(process.env.URL_PREFIX + "favourites", favRouter);
 // app.use(productRouter)
-
